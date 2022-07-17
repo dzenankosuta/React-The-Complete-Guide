6d903cb52f6ba79ee6a031acf84ab61a435f3c98
dzenankosuta <dzenan_kosuta@hotmail.com>
2022-07-17 12:53:33 +0200

    The Concept of "Composition" ("children props")

--- react-complete-guide/src/components/Expenses.js
+++ react-complete-guide/src/components/Expenses.js
@@ -1,10 +1,11 @@
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css'
+import Card from "./Card";
 
 function Expenses(props) {
   
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -25,7 +26,7 @@ function Expenses(props) {
         amount={props.items[3].amount}
         date={props.items[3].date}
       />
-    </div>
+    </Card>
   );
 }
 
