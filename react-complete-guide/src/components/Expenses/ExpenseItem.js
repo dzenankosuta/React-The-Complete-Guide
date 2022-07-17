26f0e8b300ed1f41fa045622f379e978fd7c6403
dzenankosuta <dzenan_kosuta@hotmail.com>
2022-07-17 16:38:10 +0200

    How Component Functions Are Executed

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -4,21 +4,22 @@ import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  let title = props.title;
 
   const clickHandler = () => {
-    console.log("Clicked!")
-  }
-  
+    title = "Updated!";
+  };
+
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
