2774e6eb70923ad70fa54a8a0845952fe3777f44
dzenankosuta <dzenan_kosuta@hotmail.com>
2022-08-12 12:48:40 +0200

    Using One State Instead (And What's Better)

--- react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -2,17 +2,34 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  const [enteredTitle, setEnteredTitle] = useState("");
+  // const [enteredTitle, setEnteredTitle] = useState("");
+  // const [enteredAmount, setEnteredAmount] = useState("");
+  // const [enteredDate, setEnteredDate] = useState("");
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
   const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value);
+    // setEnteredTitle(event.target.value)
+    setUserInput({
+      ...userInput,
+      enteredTitle: event.target.value,
+    });
   };
-  const [enteredAmount, setEnteredAmount] = useState("");
   const amountChangeHandler = (event) => {
-    setEnteredAmount(event.target.value);
+    // setEnteredAmount(event.target.value)
+    setUserInput({
+      ...userInput,
+      enteredAmount: event.target.value,
+    });
   };
-  const [enteredDate, setEnteredDate] = useState("");
   const dateChangeHandler = (event) => {
-    setEnteredDate(event.target.value);
+    // setEnteredDate(event.target.value)
+    setUserInput({
+      ...userInput,
+      enteredDate: event.target.value,
+    });
   };
 
   return (
