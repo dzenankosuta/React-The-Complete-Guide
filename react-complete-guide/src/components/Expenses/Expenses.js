02ca0a60f5d19e2071fda8dcf06b6bf676306c08
dzenankosuta <dzenan_kosuta@hotmail.com>
2022-08-13 14:36:03 +0200

    Outputting conditional content - 2. way

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -21,9 +21,8 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.length === 0 ? (
-          <p>No expenses found.</p>
-        ) : (
+        {filteredExpenses.length === 0 && <p>No expenses found.</p>}
+        {filteredExpenses.length > 0 &&
           filteredExpenses.map((expense) => (
             <ExpenseItem
               key={expense.id}
@@ -31,8 +30,7 @@ const Expenses = (props) => {
               amount={expense.amount}
               date={expense.date}
             />
-          ))
-        )}
+          ))}
       </Card>
     </div>
   );
